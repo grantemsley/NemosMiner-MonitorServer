d45889ff493d5785b0442a94b327dff4bd390fcf
Grant Emsley <grant@emsley.ca>
2018-09-21 22:29:40 -0400

    Landing page

--- web/index.php
+++ web/index.php
@@ -1,6 +1,36 @@
-Not implemented yet.
 <?php
 require_once('private/functions.php');
 cleanup();
-print get_user_count();
+$stats = get_user_count();
 ?>
+
+<?php include 'includes/head.html';?>
+<header class="text-white">
+  <div class="container text-center">
+    <h1>Welcome to the NemosMiner Monitoring Server</h1>
+    <p class="lead">Keep tabs on all your mining rigs from one place!</p>
+    <p>Currently monitoring <?=$stats['workers']?> rigs for <?=$stats['users']?> users</p>
+  </div>
+</header>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6 text-center p-4">
+      <h2 class="pt-4">Already setup?</h2>
+      <p class="lead">Enter your user id to view your workers</p>
+      <form>
+        <div class="form-group">
+          <label for="userid">User ID</label>
+          <input type="text" class="form-control" id="userid">
+        </div>
+        <button type="submit" class="btn btn-primary">View workers</button>
+      </form>  
+            
+    </div>
+  
+    <div class="col-md-6 bg-light text-center p-4">
+      <h2 class="pt-4">Need to get started?</h2>
+      <p class="lead">Follow <a href="setup.php">this guide</a> to get started.</p>
+    </div>
+  </div>
+</div>
+<?php include 'includes/foot.html';?>
