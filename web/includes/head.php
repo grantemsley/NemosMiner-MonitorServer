c716dfdd9f5204777e1803ecc50dac5a8c292cc4
Grant Emsley <grant@emsley.ca>
2018-09-26 11:54:27 -0400

    Fix typo that broke menu on mobile

--- web/includes/head.php
+++ web/includes/head.php
@@ -21,7 +21,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id=navbarResponsive">
+        <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
             <li class="nav-item"><a class="nav-link" href="https://github.com/nemosminer/NemosMiner">Github</a></li>
